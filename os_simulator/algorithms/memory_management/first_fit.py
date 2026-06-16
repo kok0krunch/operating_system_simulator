822a668264e5d37ac52e03a73c7337b557d36160
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2026-06-16 17:02:32 +0800

    feat (first-fit): Added memory type characteristic for MFT and MVT. Also created possible definitions needed. May change later.

--- os_simulator/algorithms/memory_management/first_fit.py
+++ os_simulator/algorithms/memory_management/first_fit.py
@@ -1,7 +1,17 @@
 # First-Fit Memory Management Algorithm
 
 class FirstFit:
-    def __init__(self, memory_sizes, process_sizes, burst_times):
+    def __init__(self, memory_sizes, process_sizes, burst_times, memory_type):
         self.memory_sizes = memory_sizes
         self.process_sizes = process_sizes
         self.burst_times = burst_times
+        self.memory_type= memory_type
+
+    def user_input(self):
+       pass
+
+    def main_logic(self):
+        pass
+
+    def working_algorithm (self):
+        pass
