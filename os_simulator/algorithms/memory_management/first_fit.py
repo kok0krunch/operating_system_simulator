7acf5b64103eae40b9a584ffdf9162f3d042c7bd
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2026-06-16 17:01:31 +0800

    feat (first-fit): Created first fit class with expected first fit characteristics.

--- os_simulator/algorithms/memory_management/first_fit.py
+++ os_simulator/algorithms/memory_management/first_fit.py
@@ -1,1 +1,7 @@
 # First-Fit Memory Management Algorithm
+
+class FirstFit:
+    def __init__(self, memory_sizes, process_sizes, burst_times):
+        self.memory_sizes = memory_sizes
+        self.process_sizes = process_sizes
+        self.burst_times = burst_times
