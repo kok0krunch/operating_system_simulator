98ce183c64d33801a1044d978cc9d1ccef57ab48
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2026-06-16 17:55:06 +0800

    feat (first-fit): Removed memory_type from characteristics of first-fit class. Also fixed indentation of lines of code for mvt_logic and added input for mft_logic

--- os_simulator/algorithms/memory_management/first_fit.py
+++ os_simulator/algorithms/memory_management/first_fit.py
@@ -1,30 +1,33 @@
 # First-Fit Memory Management Algorithm
 
 class FirstFit:
-    def __init__(self, memory_size, process_size, burst_time, memory_type):
+    def __init__(self, memory_size, process_size, burst_time,):
         self.memory_size = memory_size
         self.process_size = process_size
         self.burst_time = burst_time
-        self.memory_type = memory_type
 
     def mft_logic(self):
-        pass
+        while True:
+            try:
+                memory_size_input = input("Enter partitions separated with comma (ex. 1,2,3): ").strip()
+            except:
+                pass
 
     def mvt_logic(self):
          while True:
-                try:
-                    memory_size_input = input("Enter memory block sizes separated by comma (e.g., 100,200,300): ").strip()
-                    if int(memory_size_input)>0:
-                      pass
-                    else:
-                        return ValueError
-                except:
-                    if memory_size_input<=0:
-                        print("Memory block size inserted is negative. Please input a positive number.")
-                        continue
-                    else:
-                        print("Memory block size inserted is not a number. Please type a number")
-                        continue
+            try:
+                memory_size_input = input("Enter total memory block size: ").strip()
+                if int(memory_size_input)>0:
+                    pass
+                else:
+                    return ValueError
+            except:
+                if memory_size_input<=0:
+                    print("Memory block size inserted is negative. Please input a positive number.")
+                    continue
+                else:
+                    print("Memory block size inserted is not a number. Please type a number")
+                    continue
 
 # definitions
 def user_input():
