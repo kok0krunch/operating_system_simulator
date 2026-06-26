6ca2d02166e1a0282d8e53fb76384113b341ad50
talamor-shella <shellatalamor@gmail.com>
2026-06-26 23:00:44 +0800

    feat(dm_pygame): integrate scan disk scheduling algorithm option

--- os_simulator/algorithms/disk_management/dm_pygame.py
+++ os_simulator/algorithms/disk_management/dm_pygame.py
@@ -8,14 +8,14 @@ import os
 try:
     from .fcfs_disk import fcfs_menu
     from .sstf import sstf_menu
-    #from .scan import scan_menu
+    from .scan import scan_menu
     from .cscan import cscan_menu
     #from .look import look_menu
     from .clook import clook_menu
 except ImportError:
     from fcfs_disk import fcfs_menu
     from sstf import sstf_menu
-    #from scan import scan_menu
+    from scan import scan_menu
     from cscan import cscan_menu
     #from look import look_menu
     from clook import clook_menu
@@ -55,7 +55,7 @@ def main_disk_menu():
     menu_options = [
         ("[1] First-Come, First-Served (FCFS)", fcfs_menu),
         ("[2] Shortest Seek Time First (SSTF)", sstf_menu),
-        #("[3] SCAN Scheduling (Elevator)", scan_menu),
+        ("[3] SCAN Scheduling (Elevator)", scan_menu),
         ("[4] Circular SCAN Scheduling (C-SCAN)", cscan_menu),
         #("[5] LOOK Scheduling Algorithm", look_menu),
         ("[6] Circular LOOK Scheduling (C-LOOK)", clook_menu)
