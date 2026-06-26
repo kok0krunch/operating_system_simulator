52da121c9f4615226c9e0ec1d9730f3ee4ced1fb
kok0krunch <amaliakadoi@gmail.com>
2026-06-27 01:10:11 +0800

    refactor(dm_pygame): removed 'algorithm' for look choice

--- os_simulator/algorithms/disk_management/dm_pygame.py
+++ os_simulator/algorithms/disk_management/dm_pygame.py
@@ -57,7 +57,7 @@ def main_disk_menu():
         ("[2] Shortest Seek Time First (SSTF)", sstf_menu),
         ("[3] SCAN Scheduling (Elevator)", scan_menu),
         ("[4] Circular SCAN Scheduling (C-SCAN)", cscan_menu),
-        ("[5] LOOK Scheduling Algorithm", look_menu),
+        ("[5] LOOK Scheduling", look_menu),
         ("[6] Circular LOOK Scheduling (C-LOOK)", clook_menu)
     ]
 
